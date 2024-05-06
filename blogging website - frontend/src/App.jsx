55bb5c568246c1453a4278baa96e05cd3eb0787c
Aayush701 <ayushghimire7011@gmail.com>
2024-05-06 17:08:59 +0545

    Navbar and setting up routes finished

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1>MERN Blogging website</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="signin" element={<h1>Sign in page</h1>} />
+          <Route path="signup" element={<h1>Signup page</h1>} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ blogging website - frontend/src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import logo from "../imgs/logo.png";
+import { Link, Outlet } from "react-router-dom";
+const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <>
+      <nav className="navbar">
+        {/* logo */}
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} alt="" />
+        </Link>
+        {/* search bar  (hidden in mobile format)*/}
+        <div
+          className={
+            "absolute bg-white w-full left-0 top-full mt-0 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
+            (isVisible ? "show" : "hide")
+          }
+        >
+          <input
+            type="text"
+            placeholder="Search "
+            className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 "
+          />
+          <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey" />
+        </div>
+
+        {/* search bar icon when in mobile format */}
+        <div className="flex items-center gap-3 md:gap-6 ml-auto">
+          <button
+            type="button"
+            onClick={() => setIsVisible((currentVal) => !currentVal)}
+            className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+          >
+            <i className="fi fi-rr-search md:hidden text-2xl bg-grey w-12 h-12 rounded-full flex items-center justify-center" />
+          </button>
+        </div>
+        {/* edit and write or post */}
+        <Link to="/editor" className="hidden md:flex gap-2 link rounded-md">
+          <i className="fi fi-rr-file-edit" />
+          <span>Write</span>
+        </Link>
+
+        {/* sign in or sign out bar */}
+        <Link to="/signin" className="btn-dark py-2">
+          Sign In
+        </Link>
+        <Link to="/signup" className="btn-light py-2 hidden md:block">
+          Sign Up
+        </Link>
+      </nav>
+      {/* outlet is required for the nested route in app section */}
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbar;
